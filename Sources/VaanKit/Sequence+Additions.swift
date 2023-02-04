919ede5a20e5df79567d00db526f56d666b5dabf
Imthath <imthath.m@hotmail.com>
2023-02-04 07:03:44 +0530

    Add list builder

--- Sources/VaanKit/Sequence+Additions.swift
+++ Sources/VaanKit/Sequence+Additions.swift
@@ -17,3 +17,39 @@ public extension Sequence {
     }
   }
 }
+
+@resultBuilder public struct ListBuilder<I> {
+  public typealias Aggregate = [I]
+
+  @inlinable public static func buildBlock(_ components: Aggregate...) -> Aggregate {
+    buildArray(components)
+  }
+
+  @inlinable public static func buildOptional(_ component: Aggregate?) -> Aggregate {
+    component ?? []
+  }
+
+  @inlinable public static func buildEither(first component: Aggregate) -> Aggregate {
+    component
+  }
+
+  @inlinable public static func buildEither(second component: Aggregate) -> Aggregate {
+    component
+  }
+
+  @inlinable public static func buildArray(_ components: [Aggregate]) -> Aggregate {
+    components.flatMap { $0 }
+  }
+
+  @inlinable public static func buildExpression(_ expression: I) -> Aggregate {
+    [expression]
+  }
+
+  @inlinable public static func buildExpression(_ expression: I?) -> Aggregate {
+    expression.map { [$0] } ?? []
+  }
+
+  @inlinable public static func buildExpression<S>(_ expression: S) -> Aggregate where S: Sequence, S.Element == I {
+    Array(expression)
+  }
+}
