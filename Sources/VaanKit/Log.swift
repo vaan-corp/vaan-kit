7ce8566250902a27243b1c51b64f3cad2870ae4a
Imthath <imthath.m@hotmail.com>
2023-02-09 15:37:09 +0530

    Add logger

---
+++ Sources/VaanKit/Log.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Imthathullah on 09/02/23.
+//
+
+import Foundation
+
+public enum LogLevel {
+  case info
+  case warning
+  case error
+}
+
+public func log(_ error: Error,
+                file: StaticString = #fileID,
+                function: StaticString = #function,
+                line: UInt = #line,
+                column: UInt = #column) {
+  log(error.localizedDescription, .error, file: file, function: function, line: line, column: column)
+}
+
+public func log(_ message: String,
+                _ level: LogLevel = .info,
+                file: StaticString = #fileID,
+                function: StaticString = #function,
+                line: UInt = #line,
+                column: UInt = #column) {
+  let emoji: String = {
+    switch level {
+    case .info: return "ℹ️ℹ️ℹ️"
+    case .warning: return "⚠️⚠️⚠️"
+    case .error: return "💥💥💥"
+    }
+  }()
+  print(emoji, file, function, line, column, message, emoji)
+}
