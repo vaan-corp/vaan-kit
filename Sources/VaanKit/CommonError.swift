03609d4901852c3f44506afd710c3ccb97715632
Imthath <imthath.m@hotmail.com>
2023-03-06 18:07:12 +0530

    Improve error handling

--- Sources/VaanKit/CommonError.swift
+++ Sources/VaanKit/CommonError.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 public struct CommonError: Error {
-  let message: String
+  public let message: String
+  public let underlingError: Error?
 
-  public init(_ message: String) {
+  public init(_ message: String, underlyingError: Error? = nil) {
     self.message = message
+    self.underlingError = underlyingError
   }
 }
